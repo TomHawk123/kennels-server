da49ca217962c421c5bc1f50b9d42aa9991ed061
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-04-08 13:19:10 -0500

    added DELETE functions

--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
-from .animal_requests import get_all_animals, create_animal
-from .location_requests import get_all_locations, create_location
-from .customer_requests import get_all_customers, create_customer
-from .employee_requests import get_all_employees, create_employee
+from .animal_requests import get_all_animals, create_animal, delete_animal
+from .location_requests import get_all_locations, create_location, delete_location
+from .customer_requests import get_all_customers, create_customer, delete_customer
+from .employee_requests import get_all_employees, create_employee, delete_employee

aa6f172b70f1c987944c5af4ae81af620bd5c280
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-04-08 11:18:57 -0500

    added ability to POST new objects

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views.animal_requests import get_single_animal, get_all_animals
-from views.customer_requests import get_all_customers, get_single_customer
-from views.employee_requests import get_all_employees, get_single_employee
-from views.location_requests import get_all_locations, get_single_location
+import json
+from tkinter import N
+from views.animal_requests import get_single_animal, get_all_animals, create_animal
+from views.customer_requests import get_all_customers, get_single_customer, create_customer
+from views.employee_requests import get_all_employees, get_single_employee, create_employee
+from views.location_requests import get_all_locations, get_single_location, create_location
 
 
 # Here's a class. It inherits from another class.
@@ -93,7 +95,7 @@ class HandleRequests(BaseHTTPRequestHandler):
             if id is not None:
                 response = f"{get_single_location(id)}"
             else:
-                response = f"{get_all_locations}"
+                response = f"{get_all_locations()}"
                 # In Python, this is a list of dictionaries
                 # In JavaScript, you would call it an array of objects
 
@@ -119,15 +121,44 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+        new_location = None
+        new_employee = None
+        new_customer = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        if resource == "locations":
+            new_location = create_location(post_body)
+
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
+        self.wfile.write(f"{new_location}".encode())
+        self.wfile.write(f"{new_employee}".encode())
+        self.wfile.write(f"{new_customer}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
-
     def do_PUT(self):
         """Handles PUT requests to the server
         """

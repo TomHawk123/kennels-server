0076df347864901c05c51a0dc99f6c5dd58aa33a
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-04-09 15:49:51 -0500

    new dir w/ modulescontaining classes for location, animal, customer, and employee, .py

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -83,6 +83,6 @@ def update_animal(id, new_animal):
     # you can access the index value of each item.
     for index, animal in enumerate(ANIMALS):
         if animal["id"] == id:
-            #Found the animal. Update the value
+            # Found the animal. Update the value
             ANIMALS[index] = new_animal
             break
